408bd3f71cfdf2df3a8ff72549330a01263eacea
Shamolet <crossfit2203@gmail.com>
2023-03-29 10:20:02 +0300

    Admin part отказывается стартовать

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,45 +1,7 @@
-from datetime import datetime
-from werkzeug.urls import url_parse
-
-from app import db
 from app.admin import adm
-from flask import render_template, redirect, url_for, flash, request
-from app.constants import USER
-from app.forms.auth_forms import LoginForm
-from flask_login import login_user, logout_user, login_required, current_user
-from app.models.models import User
+from flask import render_template
 
-
-# @adm.before_app_request
-# def before_request():
-#     if current_user.is_authenticated:
-#         if not current_user.has_role("admin"):
-#             return redirect(url_for('adm.admin_logout'))
-#         else:
-#             return redirect(url_for('adm.admin_login'))
-
-
-@adm.route('/admin/login', methods=['GET', 'POST'])
-@login_required
+@adm.route('/admin', methods=['GET', 'POST'])
 def admin_login():
-    if current_user.is_authenticated:
-        return redirect(url_for('adm.admin'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data,
-                                    role=USER.ADMIN).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Неверный пароль и/или имя пользователя')
-            return redirect(url_for('adm.admin_login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('adm.admin_login')
-        return redirect(next_page)
-    return render_template('admin.admin.html', title='Админ-панель', form=form)
 
-
-@adm.route('admin/logout')
-def admin_logout():
-    logout_user()
-    return redirect(url_for('admin.admin_login'))
+    return render_template('admin/admin.html', title='Войти в админку')
